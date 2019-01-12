2ebb9c3cdd04c3eb3498dc2d54f9533e90d8693e
Mike Lazer-Walker <michael@lazerwalker.com>
2019-01-12 21:43:15 +0100

    Add video pipeline, and load those videos via a component that's aware of the pipeline

--- src/components/Cinemagraph.tsx
+++ src/components/Cinemagraph.tsx
@@ -7,8 +7,8 @@ interface Props {
 export default (props: Props): React.ReactElement<Props> => {
   return (
     <video autoPlay loop muted playsInline>
-      <source src={`${props.file}.webm`} type="video/webm" />
-      <source src={`${props.file}.mov`} type="video/mp4" />
+      <source src={`cinemagraphs/${props.file}.webm`} type="video/webm" />
+      <source src={`cinemagraphs/${props.file}.mov`} type="video/mp4" />
     </video>
   )
 }
