82f4cd561da14641c6805d785c1b4f52c89264ed
Mike Lazer-Walker <michael@lazerwalker.com>
2019-01-12 22:31:32 +0100

    Play audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, SyntheticEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Cinemagraph from './components/Cinemagraph';
 
-class App extends Component {
+interface State {
+  index: number
+}
+
+class App extends Component<{}, State> {
+  videos = [
+    "bed",
+    "shower",
+    "bike",
+    "ubahn"
+  ]
+
+  constructor(props: any) {
+    super(props)
+    this.state = { index: 0 }
+  }
+
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.onKeyDown)
+    window.addEventListener('keyup', this.onKeyUp)
+  }
+
   render() {
     return (
-      <div className="App">
-        <Cinemagraph file="bed" />
+      <div className="App" >
+        <Cinemagraph file={this.videos[this.state.index]} />
       </div>
     );
+  }
+
+  onKeyDown = (e: Event) => {
+    const index = (this.state.index >= this.videos.length ? 0 : this.state.index + 1)
+    console.log("Changin index", index)
+    this.setState({ index })
+  }
+
+  onKeyUp = (e: Event) => {
+    console.log(e)
   }
 }
 
--- src/components/Cinemagraph.tsx
+++ src/components/Cinemagraph.tsx
@@ -4,11 +4,38 @@ interface Props {
   file: String
 }
 
-export default (props: Props): React.ReactElement<Props> => {
-  return (
-    <video autoPlay loop muted playsInline>
-      <source src={`cinemagraphs/${props.file}.webm`} type="video/webm" />
-      <source src={`cinemagraphs/${props.file}.mov`} type="video/mp4" />
-    </video>
-  )
+export default class Cinemagraph extends React.Component<Props> {
+  private videoRef = React.createRef<HTMLVideoElement>()
+  private audioRef = React.createRef<HTMLAudioElement>()
+  private bgAudioRef = React.createRef<HTMLAudioElement>()
+
+  render() {
+    if (this.videoRef.current) {
+      this.videoRef.current.load()
+    }
+    if (this.audioRef.current) {
+      this.audioRef.current.load()
+    }
+
+    if (this.bgAudioRef.current) {
+      this.bgAudioRef.current.load()
+    }
+
+    return (
+      <div>
+        <video className='cinemagraph' autoPlay loop muted playsInline ref={this.videoRef}>
+          <source src={`cinemagraphs/${this.props.file}.webm`} type="video/webm" />
+          <source src={`cinemagraphs/${this.props.file}.mov`} type="video/mp4" />
+        </video>
+
+        <audio autoPlay loop ref={this.bgAudioRef}>
+          <source src={`bgaudio/${this.props.file}.mp3`} type="audio/mp3" />
+        </audio>
+
+        <audio autoPlay ref={this.audioRef}>
+          <source src={`dialog/${this.props.file}.mp3`} type="audio/mp3" />
+        </audio>
+      </div>
+    )
+  }
 }
