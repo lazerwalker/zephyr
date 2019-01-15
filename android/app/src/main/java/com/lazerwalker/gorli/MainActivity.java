f8cc51cb2d98fb919354b54f3c9419268a64e59f
Mike Lazer-Walker <michael@lazerwalker.com>
2019-01-15 22:53:20 +0100

    Add initial wonky Android app

---
+++ android/app/src/main/java/com/lazerwalker/gorli/MainActivity.java
@@ -0,0 +1,23 @@
+package com.lazerwalker.gorli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("http://ono-sendai.local:3000");
+
+    }
+}
